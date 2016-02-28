c5c4a1c154e769a046c596c777b1b5d720b9e415
vdruta <mrovidius@yahoo.com>
2016-02-28 23:11:24 +0200

    deleted for w
    deleted for w

--- rpg/src/ro/vdruta/character/Villain.java
+++
@@ -1,9 +0,0 @@
-package ro.vdruta.character;
-/**
- * Created by MM on 2016-02-23.
- */
-public class Villain extends Character {
-    public Villain(String name, int level){
-        super(name, level);
-    }
-}
